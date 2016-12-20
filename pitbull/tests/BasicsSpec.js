772d7167c82ecac6e3b452d1efe8fb56a4b06fc6
karthik.c <karthik.c@lnttechservices.com>
2016-12-20 08:22:30 +0530

    test

---
+++ pitbull/tests/BasicsSpec.js
@@ -0,0 +1,7 @@
+describe('hola describe',function(){
+    it('hello world test case',function(){
+        console.log('Hello World');
+        var a = 'Hello World';
+        expect(a).toBe('Hello World');
+    });
+});
